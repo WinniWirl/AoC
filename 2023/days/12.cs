31fe5dacb141ca1db7986bb8beed24d222012b0c
Vincent <vincent.wirl@googlemail.com>
2023-12-12 07:46:29 +0100

    added day12 part 1

---
+++ 2023/days/12.cs
@@ -0,0 +1,60 @@
+using System.Text.RegularExpressions;
+using AoC_Day;
+using Helper;
+
+namespace AoC2023_Day12
+{
+    class AOCProgram : AoCDay, ISolvable
+    {
+        public void SolvePart1()
+        {
+            List<string[]> input = Helper.Helper.getInputAsLinesOfCurrentDay(day).Select(l => l.Split(" ")).ToList();
+            int result = 0;
+
+            foreach (string[] line in input)
+            {
+                List<int> commands = line[1].Split(",").Select(int.Parse).ToList();
+                result += generateAllPossibleCombinationsFor(line[0]).Where(comb => matchesCommand(comb, commands)).Count();
+            }
+
+            Console.WriteLine($"Solution Day {day} Part 1: {result}");
+        }
+
+        public bool matchesCommand(string input, List<int> commands){
+            string[] groupsOfHashtags = input.Split(".", StringSplitOptions.RemoveEmptyEntries);
+            if(groupsOfHashtags.Length != commands.Count) return false;
+            for (int i = 0; i < groupsOfHashtags.Length; i++)
+            {
+                if(groupsOfHashtags[i].Length != commands[i]) return false;
+            }
+            return true;
+        }
+
+        public List<string> generateAllPossibleCombinationsFor(string input){
+            Console.WriteLine($"generating variant for: {input}");
+
+            List<string> result = [];
+            result.Add(input);
+            
+            while(result[0].Contains('?')){
+                List<string> tmp = [];
+                for (int i = 0; i < result.Count; i++)
+                {
+                    Match m = Regex.Match(result[i], @"\?");
+                    char[] chars = result[i].ToCharArray();
+                    chars[m.Index] = '.';
+                    tmp.Add(new string(chars));
+                    chars[m.Index] = '#';
+                    tmp.Add(new string(chars));
+                }
+                result = tmp;
+            }
+            return result;
+        }
+
+        public void SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
